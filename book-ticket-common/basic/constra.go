0baccdd85105a1e00313ddb14bfd3fa9040be896
qianxunke <736567805@qq.com>
2019-12-21 08:37:16 +0800

    提交

--- book-ticket-common/basic/constra.go
+++ book-ticket-common/basic/constra.go
@@ -3,16 +3,10 @@ package basic
 const name_space = "com.book.ticket"
 
 //抢票服务
-const BookTicketService = name_space + ".srv.book_ticket_srv"
-
-//订单服务
-const OrderService = name_space + ".srv.order"
+const BookTicketService = name_space + ".srv.book"
 
 //鉴权服务
 const AuthService = name_space + ".srv.auth"
-
-//支付服务
-const PaymentService = name_space + ".srv.payment"
 
 //用户服务
 const UserService = name_space + ".srv.user"
