bac733b282617426b707da7c0b2f31a95f304316
qianxunke <736567805@qq.com>
2019-12-21 17:47:23 +0800

    提交

--- book-ticket-common/basic/common/const.go
+++ book-ticket-common/basic/common/const.go
@@ -8,5 +8,5 @@ const (
 	Topic_Product_Es_Add        = "com.surprise.shop.topic.product.add"
 	Topic_Product_Es_Delete     = "com.surprise.shop.topic.product.delete"
 	Topic_Product_Es_Update     = "com.surprise.shop.topic.product.update"
-	ControlCenterAddress        = "127.0.0.1:9600"
+	ControlCenterAddress        = "172.17.0.12:9600"
 )
